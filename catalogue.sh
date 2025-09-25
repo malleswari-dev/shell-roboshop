bd4866f72dfba4295a8e09060d6893c1333bdf32
malleswari <maddikirimalleswari4@gmail.com>
2025-09-25 16:47:50 +0530

    logs

--- catalogue.sh
+++ catalogue.sh
@@ -80,7 +80,7 @@ VALIDATE $? "copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "install mongodb client"
 
-INDEX=$(mongosh mongodb.malleswari.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
