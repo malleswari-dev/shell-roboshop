a2bf2f8a605f241bee776d966e7480c6dea92db9
malleswari <maddikirimalleswari4@gmail.com>
2025-09-25 16:22:52 +0530

    logs

--- catalogue.sh
+++ catalogue.sh
@@ -80,8 +80,15 @@ VALIDATE $? "copy mongo repo"
 dnf install mongodb-mongosh -y &>> LOG_FILE
 VALIDATE $? "install mongodb client"
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js &>> LOG_FILE
-VALIDATE $? "load catalogue products"
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+#mongosh --host $MONGODB_HOST </app/db/master-data.js &>> LOG_FILE
+#VALIDATE $? "load catalogue products"
 
 systemctl restart catalogue
 VALIDATE $? "restart catalogue"
