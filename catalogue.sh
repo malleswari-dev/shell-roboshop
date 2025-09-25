c2cfe73b780139b1237a597d705c72250a7fb02d
malleswari <maddikirimalleswari4@gmail.com>
2025-09-25 15:47:45 +0530

    logs

---
+++ catalogue.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# /var/log/shell-practice/16-logs/log
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at:$(date)"  | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:please run this script with root privilage"
+    exit 1 # failure is othher than 0
+fi     
+# functions receives inputs through args like shell script args.
+VALIDATE () { 
+    if [ $1 -ne 0 ] ; then
+        echo -e "  $2 is $R failure $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e " $2 is $G success $N"  | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> LOG_FILE
+VALIDATE $? "disable nodejs"
+
+dnf module enable nodejs:20 -y &>> LOG_FILE
+VALIDATE $? "enable nodejs"
+
+dnf install nodejs -y &>> LOG_FILE
+VALIDATE $? "install nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> LOG_FILE
+VALIDATE $? "creating system user"
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> LOG_FILE
+VALIDATE $? "download code"
+cd /app
+VALIDATE $? "changing app directory" 
+
+unzip /tmp/catalogue.zip &>> LOG_FILE
+VALIDATE $? "unzip code"
+
+npm install &>> LOG_FILE
+VALIDATE $? "install dependencies" 
+
+cp catalogue.service /etc/systemd/system/catalogue.service &>> LOG_FILE
+VALIDATE $? "copy systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue
+VALIDATE $? "enable catologue" 
+
+systemctl start catalogue
+VALIDATE $? "start catalogue"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOG_FILE
+VALIDATE $? "copy mongo repo"
+
+dnf install mongodb-mongosh -y &>> LOG_FILE
+VALIDATE $? "install mongodb client"
+
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js &>> LOG_FILE
+VALIDATE $? "load catalogue products"
+
+systemctl restart catalogue
+VALIDATE $? "restart catalogue"
--- mongodb.sh
+++ mongodb.sh
@@ -40,8 +40,8 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod 
 VALIDATE $? "Start MongoDB"
 
-# sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-# VALIDATE $? "Allowing remote connections to MongoDB"
+ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+ VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
 VALIDATE $? "Restarted MongoDB"
