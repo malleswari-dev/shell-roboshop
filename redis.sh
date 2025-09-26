2c086f1a20894295ed996c1791067f3bbc8515fb
malleswari <maddikirimalleswari4@gmail.com>
2025-09-26 17:21:44 +0530

    logs

--- redis.sh
+++ redis.sh
@@ -9,8 +9,8 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-# /var/log/shell-practice/16-logs/log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-practice/16-logs/log
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at:$(date)"  | tee -a $LOG_FILE
@@ -46,3 +46,7 @@ VALIDATE $? "enable redis"
 
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "start redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME))
+echo -e "script executed in:$Y  $TOTAL_TIME seconds $N
